1713ab8e91619d02bac8a0f5f0a587a2651bc2cc
Mikhail Arkhipov <arkhipov-ms@ya.ru>
2020-09-16 22:19:25 +0300

    Templating

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,20 +10,6 @@ class MainController extends AppController
 {
     public function indexAction()
     {
-        $posts = \R::findAll('test');
-        $post = \R::findOne('test', 'id = ?', [2]);
         $this->setMeta('Главная страница', 'Описание ...', 'Ключевики...');
-        $name = 'John';
-        $age = 55;
-        $names = ['1', '222', 'Mike'];
-//        Cache::set('test', $names);
-//        Cache::delete('test');
-        $data = Cache::get('test');
-        if(!$data){
-            Cache::set('test', $names);
-        }
-
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
